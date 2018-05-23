e75f4ba7a7127315ee5a957e7aa5c5e7fd240c52
cycjimmy <cycjimmy@gmail.com>
2018-05-23 11:13:00 +0800

    task01 ok

--- app/main.js
+++ app/main.js
@@ -12,4 +12,21 @@ if (PRODUCTION) {
 // web page init
 document.addEventListener('DOMContentLoaded', () => {
   console.log('DOMContentLoaded');
+
+  let
+    eText = document.querySelector('.text')
+    , eButton = document.querySelector('.button')
+  ;
+
+  const ACTIVE_CLASS_NAME = 'active';
+
+  eButton.addEventListener('click', () => {
+    if (eText.classList.contains(ACTIVE_CLASS_NAME)) {
+      eText.classList.remove(ACTIVE_CLASS_NAME);
+      return;
+    }
+
+    eText.classList.add(ACTIVE_CLASS_NAME);
+  });
+
 }, false);
