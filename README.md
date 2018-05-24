904ea84e8cb62c9c8a0345e546c021dd02ff30fb
cycjimmy <cycjimmy@gmail.com>
2018-05-24 17:19:56 +0800

    task06 ok

--- README.md
+++ README.md
@@ -1,9 +1,7 @@
-# IFE2018
+# No.6 - 利用 CSS animation 制作一个炫酷的 Slider
 
-个人练习代码托管仓库
-
-## create new branch based on master
-```shell
-$ git checkout -b [newBranch] master
-```
+* 分支名: designerAcademy-task06
+* [任务要求](http://ife.baidu.com/course/detail/id/33)
+* [个人练习完成源码](https://github.com/cycdpoCodeLab/ife-course-2018/tree/designerAcademy-task06)
+* [完成效果](https://cycdpocodelab.github.io/ife-course-2018/designerAcademy/task06/index.html)
 
--- app/main.js
+++ app/main.js
@@ -12,4 +12,53 @@ if (PRODUCTION) {
 // web page init
 document.addEventListener('DOMContentLoaded', () => {
   console.log('DOMContentLoaded');
+
+  const ACTIVE_CLASS_NAME = 'active';
+
+  let
+    aPics = Array.prototype.slice.call(document.querySelectorAll('.pic img'))
+    , eThumb = document.querySelector('.thumb')
+    , aThumbs = Array.prototype.slice.call(eThumb.querySelectorAll('img'))
+
+    // 默认激活第一个
+    , sActiveIndex = '1'
+
+    // 修改nZIndex使得后激活的图片总是在之前图片上
+    , nZIndex = 1
+  ;
+
+  // 初始化第一张图片
+  aPics[0].classList.add(ACTIVE_CLASS_NAME);
+  aPics[0].style.zIndex = ++nZIndex;
+  aThumbs[0].classList.add(ACTIVE_CLASS_NAME);
+
+  // 点击缩略图
+  eThumb.addEventListener('click', e => {
+    let
+      _target = e.target
+      , _index = _target.dataset.index
+    ;
+
+    // 不存在index直接跳出
+    if (!_index) {
+      return;
+    }
+
+    // 点击了正在激活状态的thumb，直接跳出
+    if (_index === sActiveIndex) {
+      return;
+    }
+
+    // thumb变化
+    aThumbs[sActiveIndex - 1].classList.remove(ACTIVE_CLASS_NAME);
+    aThumbs[_index - 1].classList.add(ACTIVE_CLASS_NAME);
+
+    // pic变化
+    aPics[sActiveIndex - 1].classList.remove(ACTIVE_CLASS_NAME);
+    aPics[_index - 1].classList.add(ACTIVE_CLASS_NAME);
+    aPics[_index - 1].style.zIndex = ++nZIndex;
+
+    // 更新sActiveIndex
+    sActiveIndex = _index;
+  });
 }, false);
