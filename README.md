09c54ccdba00aea5d1c161f95362dc15c8af27e4
cycjimmy <cycjimmy@gmail.com>
2018-05-29 17:13:57 +0800

    save

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-# 1.2 用webpack4 / parcel快速搭建开发san helloworld
+# 2.1 数据操作
 
-* 分支名: mvvmAcademy-task1.2
-* [任务要求](http://ife.baidu.com/course/detail/id/2)
-* [个人练习完成源码](https://github.com/cycdpoCodeLab/ife-course-2018/tree/mvvmAcademy-task1.2)
-* [完成效果](https://cycdpocodelab.github.io/ife-course-2018/mvvmAcademy/task1.2/index.html)
+* 分支名: mvvmAcademy-task2.1
+* [任务要求](http://ife.baidu.com/course/detail/id/4)
+* [个人练习完成源码](https://github.com/cycdpoCodeLab/ife-course-2018/tree/mvvmAcademy-task2.1)
+* [完成效果](https://cycdpocodelab.github.io/ife-course-2018/mvvmAcademy/task2.1/index.html)
 
--- app/main.js
+++ app/main.js
@@ -14,12 +14,49 @@ if (PRODUCTION) {
 
 // 定义Component
 let MyApp = san.defineComponent({
-  template: `<h1 class="title">{{ hello }}</h1>`
+  template: `
+    <section id="root">
+      <!-- 输入框 -->
+      <div class="input-wrapper">
+        <input type="text" class="input" name="{{ nameTitle }}" placeholder="{{ nameTitle }}" value="{= name =}">
+        <input type="text" class="input" name="{{ ageTitle }}" placeholder="{{ ageTitle }}" value="{= age =}">
+        <input type="text" class="input" name="{{ desTitle }}" placeholder="{{ desTitle }}" value="{= des =}">
+      </div>
+      
+      <!-- 移除信息按钮 -->
+      <button class="remove-info-btn" on-click="removeInfo">{{ removeInfoTitle }}</button>
+      
+      <!-- 信息显示区 -->
+      <div class="info-wrapper">
+        <p>姓名: {{ name }}</p>
+        <p>年龄: {{ age }}</p>
+        <p>简介: {{ des }}</p>
+      </div>
+    </section>
+  `,
+
+  removeInfo: function() {
+    console.log(this);
+    this.data.set('name', '');
+    this.data.set('age', '');
+    this.data.set('des', '');
+  }
+
+  // initData: function () {
+  //   return {
+  //     name: '',
+  //     age: '',
+  //     des: '',
+  //   };
+  // },
 });
 
 let myApp = new MyApp({
   data: {
-    hello: 'Hello World!'
+    nameTitle: '姓名',
+    ageTitle: '年龄',
+    desTitle: '简介',
+    removeInfoTitle: '移除信息',
   }
 });
 
