ba1210c0fc73141d48c5908839413f24ba6684e9
cycjimmy <cycjimmy@gmail.com>
2018-05-31 17:25:48 +0800

    task 2.3 ok

--- app/main.js
+++ app/main.js
@@ -14,14 +14,33 @@ if (PRODUCTION) {
 
 // 定义Component
 let MyApp = san.defineComponent({
-  template: `<h1 class="title">{{ hello }}</h1>`
+  template: `
+  <div class="root">
+      <div class="block" style="background: {{ isBlock1Changed ? 'red' : 'blue'}};"></div>
+      <button id="changeButton1" on-click="toggle('isBlock1Changed')">通过style方式改变</button>
+      
+      <div class="block{{ isBlock2Changed ? ' changed' : '' }}"></div>
+      <button id="changeButton2" on-click="toggle('isBlock2Changed')">通过class方式改变</button>
+  </div>
+  `,
+
+  initData: function () {
+    return {
+      isBlock1Changed: false,
+      isBlock2Changed: false
+    };
+  },
+
+  /**
+   * 切换状态
+   * @param stateName
+   */
+  toggle: function (stateName) {
+    this.data.set(stateName, !this.data.get(stateName));
+  },
 });
 
-let myApp = new MyApp({
-  data: {
-    hello: 'Hello World!'
-  }
-});
+let myApp = new MyApp();
 
 myApp.attach(document.body);
 
