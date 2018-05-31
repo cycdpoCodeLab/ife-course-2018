6c7ddd3723eb961e3e59daf3d9338d663e4ee0a6
cycjimmy <cycjimmy@gmail.com>
2018-05-31 16:26:42 +0800

    task2.2 ok

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-# 1.2 用webpack4 / parcel快速搭建开发san helloworld
+# 2.2 条件和循环
 
-* 分支名: mvvmAcademy-task1.2
-* [任务要求](http://ife.baidu.com/course/detail/id/2)
-* [个人练习完成源码](https://github.com/cycdpoCodeLab/ife-course-2018/tree/mvvmAcademy-task1.2)
-* [完成效果](https://cycdpocodelab.github.io/ife-course-2018/mvvmAcademy/task1.2/index.html)
+* 分支名: mvvmAcademy-task2.2
+* [任务要求](http://ife.baidu.com/course/detail/id/5)
+* [个人练习完成源码](https://github.com/cycdpoCodeLab/ife-course-2018/tree/mvvmAcademy-task2.2)
+* [完成效果](https://cycdpocodelab.github.io/ife-course-2018/mvvmAcademy/task2.2/index.html)
 
--- app/main.js
+++ app/main.js
@@ -4,6 +4,9 @@ import './theme/main.scss';
 // 引入san
 import san from 'san';
 
+// 引入array-findIndex polyfill
+import 'array-findIndex.polyfill';
+
 if (DEVELOPMENT) {
   console.log('Development Mode');
 }
@@ -12,14 +15,156 @@ if (PRODUCTION) {
   console.log('Production Mode');
 }
 
+
 // 定义Component
 let MyApp = san.defineComponent({
-  template: `<h1 class="title">{{ hello }}</h1>`
+  template: `
+    <div class="root">
+      <!-- 添加按钮 -->
+      <button class="add-btn" on-click="add">{{ addBtnTitle }}</button>
+      
+      <!-- 表格 -->
+      <table class="table">
+        <tr>
+          <th>{{ tableColumnTitleName }}</th>
+          <th>{{ tableColumnTitleState }}</th>
+          <th>{{ tableColumnTitleAction }}</th>
+        </tr>
+        
+        <!-- 循环体 -->
+        <tr s-for="personState in stateList">
+          <td>{{ personState.name }}</td>
+          
+          <template s-if="personState.state === 0">
+            <td>{{ stateUnreviewed }}</td>
+            <td>
+              <button on-click="review(personState)">{{ actionReview }}</button>
+            </td>
+          </template>
+          
+          <template s-else-if="personState.state === 1">
+            <td>{{ statePass }}</td>
+            <td>
+              <button on-click="delete(personState)">{{ actionDelete }}</button>
+            </td>
+          </template>
+          
+          <template s-else-if="personState.state === -1">
+            <td>{{ stateFail }}</td>
+            <td>
+              <button on-click="delete(personState)">{{ actionDelete }}</button>
+            </td>
+          </template>
+          
+          <template s-else>
+            <td></td>
+            <td></td>
+          </template>
+        </tr>
+      
+      </table>
+    </div>
+  `,
+
+  initData: function () {
+    return {
+      // id 防止重名设定的唯一性标志
+      // state 0为未审核 1为合格 -1为不合格
+      stateList: [{
+        id: 1,
+        name: '张三',
+        state: 1,
+      }, {
+        id: 2,
+        name: '李四',
+        state: -1,
+      }, {
+        id: 3,
+        name: '王五',
+        state: 0,
+      }, {
+        id: 4,
+        name: '赵六',
+        state: 0,
+      }, {
+        id: 5,
+        name: '孙七',
+        state: 0,
+      }],
+
+      nextId: 6,  // 添加记录后自增值
+    };
+  },
+
+  /**
+   * 添加一条记录
+   */
+  add: function () {
+    console.log(this);
+
+    let
+      _nextId = this.data.get('nextId')
+      , _nameStr = prompt('请输入姓名', '')
+    ;
+
+    // 姓名验证
+    let regName = /^[\u4e00-\u9fa5]{2,4}$/;
+    if (!regName.test(_nameStr)) {
+      alert('请输入正确的姓名(2~4位中文字符)');
+      return;
+    }
+
+    this.data.push('stateList', {
+      id: _nextId++,
+      name: _nameStr,
+      state: 0,
+    });
+    this.data.set('nextId', _nextId);
+  },
+
+  /**
+   * 删除一条记录
+   * @param stateObj
+   */
+  delete: function (stateObj) {
+    console.log(stateObj);
+    let _index = this.getIndex(stateObj);
+    this.data.removeAt('stateList', _index);
+  },
+
+  /**
+   * 审核
+   * @param stateObj
+   */
+  review: function (stateObj) {
+    let
+      _index = this.getIndex(stateObj)
+      , _newState = 1
+    ;
+
+    this.data.set('stateList[' + _index + '].state', _newState);
+  },
+
+  getIndex: function (stateObj) {
+    return this.data
+      .get('stateList')
+      .findIndex(state => state.id === stateObj.id);
+  }
 });
 
 let myApp = new MyApp({
   data: {
-    hello: 'Hello World!'
+    addBtnTitle: '添加',
+    tableColumnTitleName: '姓名',
+    tableColumnTitleState: '审核状态',
+    tableColumnTitleAction: '操作',
+
+    stateUnreviewed: '未审核',
+    statePass: '合格',
+    stateFail: '不合格',
+
+    actionReview: '审核',
+    actionDelete: '删除',
   }
 });
 
