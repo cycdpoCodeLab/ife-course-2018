f254c276a692e8d43e74401d490750612dd91cf6
cycjimmy <cycjimmy@gmail.com>
2018-06-06 14:39:54 +0800

    task2.6 ok

--- app/main.js
+++ app/main.js
@@ -13,11 +13,13 @@ if (PRODUCTION) {
 }
 
 import TaskMenuComponent from './task-menu/TaskMenu.component';
+import TaskCardComponent from './task-card/TaskCard.component';
 
 // 定义Component
 let MyApp = san.defineComponent({
   components: {
-    'task-menu': TaskMenuComponent
+    'task-menu': TaskMenuComponent,
+    'task-card': TaskCardComponent,
   },
   template: `
     <div class="root">
@@ -25,16 +27,55 @@ let MyApp = san.defineComponent({
       <task-menu title="{{ title }}"></task-menu>
       
       <!-- 任务卡片 -->
-      <section class="task-card-wrapper">
-      
-      </section>
+      <task-card tasks="{{ tasks }}" headerColor="{{ headerColor }}" s-ref="tasks"></task-card>
     </div>
   `
 });
 
 let myApp = new MyApp({
   data: {
-    title: '任务栏'
+    title: '任务栏',
+
+    tasks: [{
+      header: 'task1',
+      title: 'slot',
+      content: 'xxxxxx',
+      time: '2018-01-01',
+    }, {
+      header: 'task2',
+      title: 'slot',
+      content: 'xxxxxx',
+      time: '2018-01-01',
+    }, {
+      header: 'task3',
+      title: 'slot',
+      content: 'xxxxxx',
+      time: '2018-01-01',
+    }, {
+      header: 'task4',
+      title: 'slot',
+      content: 'xxxxxx',
+      time: '2018-01-01',
+    }, {
+      header: 'task5',
+      title: 'slot',
+      content: 'xxxxxx',
+      time: '2018-01-01',
+    }, {
+      header: 'task6',
+      title: 'slot',
+      content: 'xxxxxx',
+      time: '2018-01-01',
+    }],
+
+    headerColor: {
+      task1: 'black',
+      task2: 'blue',
+      task3: 'green',
+      task4: 'darkred',
+      task5: 'purple',
+      task6: 'red',
+    }
   }
 });
 
--- app/task-card/TaskCard.component.js
+++ app/task-card/TaskCard.component.js
@@ -4,14 +4,19 @@ import './taskCard.scss';
 
 export default san.defineComponent({
   template: `
-  <template class="task-card">
-    
+  <template class="task-card-wrapper">
+    <slot s-for="item in tasks" name="{{item.header}}" var-header="item.header" var-title="item.title" var-content="item.content" var-time="item.time">
+      <div class="task-card">
+        <h3 style="{{ headerColor[item.header] ? 'color: ' + headerColor[item.header] +';' : '' }}">{{ header }}</h3>
+        
+        <section class="content">
+          <p>标题: {{ title }}</p>
+          <p>内容: {{ content }}</p>
+          <p>时间: {{ time }}</p>
+        </section>
+      </div>
+    </slot>
   </template>
   `,
-
-  initData() {
-    return {
-      title: ''
-    };
-  },
 });
+
