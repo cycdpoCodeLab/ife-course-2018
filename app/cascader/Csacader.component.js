4a4a59e94be66fa7b5bdca43b8856e1ccffbfd06
cycjimmy <cycjimmy@gmail.com>
2018-06-12 11:16:42 +0800

    update

--- app/cascader/Csacader.component.js
+++ app/cascader/Csacader.component.js
@@ -14,14 +14,15 @@ export default san.defineComponent({
       class="input{{ isBoxShow ? ' box-show' : ''}}"
       on-click="toggle"
     >
-      <span>{{ value }}</span>
+      <span>{{ value ? value : '请选择' }}</span>
     </div>
     
     <div
       s-if="isBoxShow"
+      s-transition="trans.selectorBox"
       class="selector-box"
     >
-      <ul-selector options="{{ options }}" selectedOptions="{{ selectedOptions }}"></ul-selector>
+      <ul-selector options="{{ options }}" selectedOptions="{{ selectedOptions }}" value="{{ selectedOptions[0] }}"></ul-selector>
     </div>
   </div>
   `,
@@ -30,7 +31,7 @@ export default san.defineComponent({
     return {
       options: [],
       selectedOptions: [],
-      value: '请选择',
+      value: '',
       isBoxShow: false,
     };
   },
@@ -40,19 +41,53 @@ export default san.defineComponent({
       let
         _selectedOptions = arg.value
         , _option = this.data.get('options')
-
-        // 把value数组映射成label数组
-        , _aSelectedLabel = _selectedOptions.map(sValue => {
-          let _target = _option.filter(obj => obj.value === sValue)[0];
-          _option = _target.children;
-          return _target.label;
-        })
       ;
+
+      // 把value数组映射成label数组
+      let _aSelectedLabel = _selectedOptions.map(sValue => {
+        let _target = _option.filter(obj => obj.value === sValue)[0];
+        _option = _target.children;
+        return _target.label;
+      });
 
       this.data.set('selectedOptions', _selectedOptions);
       this.data.set('value', _aSelectedLabel.join(' > '));
       this.data.set('isBoxShow', false);
-      console.log(this.data.get('selectedOptions'));
+      console.log(this.data.get('value'));
+    }
+  },
+
+  trans: {
+    selectorBox: {
+      enter: function (el, done) {
+        el.classList.add('before-enter');
+
+        // 将任务放入下一个列队，防止样式渲染异常
+        setTimeout(() => {
+          el.classList.add('enter');
+          el.classList.remove('before-enter');
+        }, 0);
+
+        el.addEventListener('transitionend', () => {
+          el.classList.remove('enter');
+          done();
+        });
+      },
+
+      leave: function (el, done) {
+        el.classList.add('before-leave');
+
+        // 将任务放入下一个列队，防止样式渲染异常
+        setTimeout(() => {
+          el.classList.add('leave');
+          el.classList.remove('before-leave');
+        }, 0);
+
+        el.addEventListener('transitionend', () => {
+          el.classList.remove('leave');
+          done();
+        });
+      }
     }
   },
 
--- app/cascader/UlSelector.component.js
+++ app/cascader/UlSelector.component.js
@@ -9,7 +9,7 @@ export default san.defineComponent({
   <div class="selector-wrapper">
     <ul class="selector">
       <li
-      class="{{ option.children ? 'has-children' : ''}}"
+      class="{{ option.children ? 'has-children' : ''}} {{ option.value === value ? 'active' : '' }}"
       s-for="option in options"
       on-click="handleClick(option)"
       >{{ option.label }}</li>
@@ -19,6 +19,7 @@ export default san.defineComponent({
       s-if="hasChildren" 
       options="{{ childrenOptions }}" 
       selectedOptions="{{ childrenSelectedOptions }}"
+      value="{{ childrenSelectedOptions[0] }}"
     ></ui-self>
   </div>
   `,
@@ -34,11 +35,27 @@ export default san.defineComponent({
     };
   },
 
+  attached() {
+    if (this.data.get('selectedOptions').length > 1) {
+      this.data.set('childrenSelectedOptions', this.data.get('selectedOptions').slice(1));
+      this.data.set('childrenOptions', this.data
+        .get('options')
+        .filter(obj => obj.value === this.data.get('value'))[0]
+        .children
+      );
+
+      this.data.set('hasChildren', true);
+    }
+
+  },
+
   handleClick(option) {
     this.data.set('value', option.value);
 
     // 先移除原先子组件，确保子组件为最新内容
     this.data.set('hasChildren', false);
+    this.data.set('childrenOptions', []);
+    this.data.set('childrenSelectedOptions', []);
 
     setTimeout(() => {
       if (option.children) {
@@ -47,7 +64,9 @@ export default san.defineComponent({
         return;
       }
 
-      this.data.push('selectedOptions', option.value);
+      this.data.set('value', option.value);
+      this.data.set('selectedOptions', [option.value]);
+
       // 向上层派发消息
       this.dispatch('UI:final_selected', this.data.get('selectedOptions'));
     }, 0);
@@ -56,10 +75,9 @@ export default san.defineComponent({
   messages: {
     'UI:final_selected': function (arg) {
       let _selectedOptions = arg.value;
-      this.data.set('selectedOptions', [this.data.get('value'), ..._selectedOptions]);
 
       // 继续上抛
-      setTimeout(() => this.dispatch('UI:final_selected', this.data.get('selectedOptions')), 0);
+      this.dispatch('UI:final_selected', [this.data.get('value'), ..._selectedOptions]);
     }
   },
 });
